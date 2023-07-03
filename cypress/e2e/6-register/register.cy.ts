de241d0f1089ff989c547eb438db2c8c10e1c241
Allyson <jandreyallyson@gmail.com>
2023-07-03 12:12:56 -0300

    test - iniciando testes na tela de registro

---
+++ cypress/e2e/6-register/register.cy.ts
@@ -0,0 +1,14 @@
+describe('Register', () => {
+
+    beforeEach(() => {
+        cy.viewport('macbook-15');
+        cy.visit('http://localhost:3000/register');
+    });
+
+    context('Quando acessar tela de registro', () => {
+
+        it('Deve exibir a tela de login corretamente', () => {
+            
+        });
+    })
+})
